79ee29d595dae705202b8ebbf8b8ae9fa05fe8bc
Alex-Yu <alexyuzhu@gmail.com>
2022-05-02 23:02:32 +0800

    feat: Develop CLI to execute the demo

--- bet_group_t_test_demo.py
+++ bet_group_t_test_demo.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import numpy as np
 from t_test import between_t_test
+import argparse
+
 
 def load_data(filename):
     df = pd.read_csv(filename)
-    s1 = df.s1[df.s1.notna()].to_numpy()
-    s2 = df.s2[df.s2.notna()].to_numpy()
     
-    return df, s1, s2
+    # Select non-null values
+    mask0 = df.iloc[:, 0].notna()
+    mask1 = df.iloc[:, 1].notna()
+    
+    # Convert to numpy arrays
+    s0, s1 = df.iloc[:, 0][mask0].to_numpy(), df.iloc[:, 1][mask1].to_numpy()
+    
+    return df, s0, s1
 
-# between-group acne medicine demo
-acne, g1, g2 = load_data('acne.csv')
-print(acne)
 
-
-# between-group avg_food_price demo
-food, g, w = load_data('avg_food_price.csv')
-print(food)
-
+def main(filename):
+    df, s0, s1 = load_data(filename)
+    print(df)
+    between_t_test(s0, s1)
+    
 
 if __name__ == '__main__':
-    between_t_test(g, w)
+    parser = argparse.ArgumentParser(description="Choose a dataset to run between-group t-Test")
+    
+    parser.add_argument('--dataset', type=str, help='the dataset you want to execute t-test on')
+    
+    args = parser.parse_args()
+    main(args.dataset)
