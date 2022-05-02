a544e1ce202c720cd79f4a432a3d1142b24f4c52
Alex-Yu <alexyuzhu@gmail.com>
2022-05-02 22:27:34 +0800

    feat: Add between-group test demo on avg_food_price

--- bet_group_t_test_demo.py
+++ bet_group_t_test_demo.py
@@ -3,12 +3,20 @@ import numpy as np
 from t_test import between_t_test
 
 
-# between-group t test
+# between-group acne medicine demo
 acne = pd.read_csv('acne.csv')
 print(acne)
 
 g1 = acne.group1[acne.group1.notna()].to_numpy()
 g2 = acne.group2[acne.group2.notna()].to_numpy()
 
+# between-group avg_food_price demo
+food = pd.read_csv('avg_food_price.csv')
+print(food)
+
+g = food.gettysburg[food.gettysburg.notna()].to_numpy()
+w = food.wilma[food.wilma.notna()].to_numpy()
+
+
 if __name__ == '__main__':
-    between_t_test(g1, g2)
+    between_t_test(g, w)
