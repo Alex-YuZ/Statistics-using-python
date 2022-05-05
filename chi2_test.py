38a4f8f9971463a25630e60ebcf9ff2014f951b5
Alex-Yu <alexyuzhu@gmail.com>
2022-05-06 01:31:36 +0800

    feat: Develop chi-square Pearson test

--- chi2_test.py
+++ chi2_test.py
@@ -28,8 +28,38 @@ def chi2_goodness_of_fit(obs, exp, alpha=.05):
     conc = "Reject the NULL" if chi_stat >= chi_critical else "Fail to reject the NULL"
     
     print(print_out.format(chi_stat, 
-                       p_val, 
-                       alpha, 
-                       chi_critical, 
-                       conc, 
-                       dof))
+                           p_val, 
+                           alpha, 
+                           chi_critical, 
+                           conc, 
+                           dof))
+    
+    
+def chi2_independence(data, alpha=0.05):
+    """Execute chi-square pearson's test on obs. and exp. data"""
+    chi_sq_statistic, p, dof, expected = chi2_contingency(data)
+    chi_sq_critical = chi2.ppf(1-alpha, dof)
+    print_out="""
+    ============== Pearson's Chi-Square Test =============
+        DoF: {0}
+        
+        Chi-Square Statistic: {1:.4f}
+        Chi-Square Critical Value (at \u03B1={3}): {5: .4f}
+        
+        P value: {2:.4f}
+        \u03B1 level: {3}
+        
+    ----------------------------------
+        Conclusion: {4}
+    ======================== END =========================
+    """
+
+    expected_df = pd.DataFrame(expected)
+    conc = "Reject the NULL" if p <= alpha else "Fail to reject the NULL"
+    
+    print(print_out.format(dof, 
+                           chi_sq_statistic, 
+                           p, 
+                           alpha, 
+                           conc, 
+                           chi_sq_critical))
