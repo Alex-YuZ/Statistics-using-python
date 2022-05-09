2c9efef718d88ac6093786c7667b9b8fca3ffa72
Alex-Yu <alexyuzhu@gmail.com>
2022-05-09 12:41:34 +0800

    refactor: Combine draw_bs_reps with bootstrap_replicate_1d

--- bootstrap.py
+++ bootstrap.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def bootstrap_replicate_1d(data, func):
-    bs_sample = np.random.choice(data, len(data))
-    return func(bs_sample)
+# def bootstrap_replicate_1d(data, func):
+#     bs_sample = np.random.choice(data, len(data))
+#     return func(bs_sample)
 
 def draw_bs_reps(data, func, size=1):
     """Draw bootstrap replicates."""
@@ -14,6 +14,7 @@ def draw_bs_reps(data, func, size=1):
 
     # Generate replicates
     for i in range(size):
-        bs_replicates[i] = bootstrap_replicate_1d(data, func)
+        bs_sample = np.random.choice(data, len(data))
+        bs_replicates[i] = func(bs_sample)
 
     return bs_replicates
