1f4a027e4d012c43adea3aded6848abea62e2621
Alex-Yu <alexyuzhu@gmail.com>
2022-05-16 14:27:13 +0800

    feat: Add cramer's v for effect size calculation

--- chi2_test.py
+++ chi2_test.py
@@ -1,6 +1,7 @@
 from scipy.stats import chisquare, chi2, chi2_contingency
 import numpy as np
 import pandas as pd
+import textwrap
 
 def chi2_goodness_of_fit(obs, exp, alpha=.05):
     """Execute chi-square goodness of fit test on obs. and exp. data"""
@@ -35,31 +36,70 @@ def chi2_goodness_of_fit(obs, exp, alpha=.05):
                            dof))
     
     
-def chi2_independence(data, alpha=0.05):
+def chi2_independence(data, inds, cols, alpha=0.05):
     """Execute chi-square pearson's test on obs. and exp. data"""
-    chi_sq_statistic, p, dof, expected = chi2_contingency(data)
-    chi_sq_critical = chi2.ppf(1-alpha, dof)
+    data = np.array(data)
+    n_size = np.sum(data)
+    k = min(data.shape)
+    
+    
+    chi2_statistic, p, dof, expected_np = chi2_contingency(data)
+    chi2_critical = chi2.ppf(1-alpha, dof)
+    
+    cramer_v = np.sqrt(chi2_statistic/(n_size*(k-1)))
+    
+    
+    obs_df = pd.DataFrame(data, index=inds, columns=cols)
+    exp_df = pd.DataFrame(expected_np, index=inds, columns=cols).round(2)
+    
+    concl = "Reject the NULL" if p < alpha else "Fail to reject the NULL"
+    
     print_out="""
     ============== Pearson's Chi-Square Test =============
-        DoF: {0}
+    *** Descriptive Summary ***
+    
+        Total Size: {0}
+        R x C: {1} x {2}
+        DoF: {3}
         
-        Chi-Square Statistic: {1:.4f}
-        Chi-Square Critical Value (at \u03B1={3}): {5: .4f}
+    *** Chi2 Statistics *** 
+    
+        Chi-Square Statistic: {4:.4f}
+        Chi-Square Critical Value (at \u03B1={5}): {6: .4f}
+        P value (when chi2={4:.2f}): {7:.4f}
+    
+    *** Effect Size ***
+    
+        Cramer's V (\u03D5): {8:.2f}
         
-        P value: {2:.4f}
-        \u03B1 level: {3}
-        
+    *** Observation and Expectation Values (Contigency Table) ***
+    
+    Observation:
+        {9}
+    
+    Expectation:
+        {10}
+    
     ----------------------------------
-        Conclusion: {4}
+    
+    Conclusion: {11}
+    
     ======================== END =========================
     """
 
-    expected_df = pd.DataFrame(expected)
-    conc = "Reject the NULL" if p <= alpha else "Fail to reject the NULL"
+    fmt_print = textwrap.dedent(print_out)
     
-    print(print_out.format(dof, 
-                           chi_sq_statistic, 
-                           p, 
+    
+    
+    print(fmt_print.format(n_size, 
+                           data.shape[0], 
+                           data.shape[1], 
+                           dof, 
+                           chi2_statistic, 
                            alpha, 
-                           conc, 
-                           chi_sq_critical))
+                           chi2_critical, 
+                           p, 
+                           cramer_v, 
+                           obs_df, 
+                           exp_df, 
+                           concl))
